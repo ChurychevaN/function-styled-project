a51f0453799feb198a9fc4858b460efcfcfa5c2b
Your Name <you@example.com>
2021-11-15 17:31:31 +0100

    all project

---
+++ src/js/modules/forms.js
@@ -0,0 +1,94 @@
+import checkNumInputs from './checkNumInputs';
+
+const forms = (state) => {
+	const form = document.querySelectorAll('form');
+	const inputs = document.querySelectorAll('input');
+
+
+	checkNumInputs('input[name="user_phone"]');
+
+
+	const message = {
+		loading: 'загрузка...',
+		success: 'спасибо! Свяжемся.',
+		failure: 'упс! Что-то пошло не так...',
+	};
+
+	const postData = async ( url, data ) => {
+		document.querySelector('.status').textContent = message.loading;
+		let res = await fetch(url, {
+			method: 'POST',
+			body: data,
+		});
+
+		return await res.text();
+	};
+
+	const clearInputs = () => {
+		inputs.forEach(item => {
+			item.value = '';
+		});
+	};
+	// for(let i = 0; i < form.length; i++) {
+	//  if(i % 2 === 0 ){}
+	// 	const item = form[i]
+	//
+	// 	item.addEventListener('submit', ( e ) => {
+	// 		e.preventDefault();
+	//
+	// 		let statusMessage = document.createElement('div');
+	// 		statusMessage.classList.add('status');
+	// 		item.appendChild(statusMessage);
+	//
+	// 		const formData = new FormData(item);
+	//
+	// 		postData('assets/server.php', formData)
+	// 		.then(res => {
+	// 			console.log(res);
+	// 			statusMessage.textContent = message.success;
+	// 		})
+	// 		.catch(() => statusMessage.textContent = message.failure)
+	// 		.finally(() => {
+	// 			clearInputs();
+	// 			setTimeout(() => {
+	// 				statusMessage.remove();
+	// 			}, 5000);
+	// 		});
+	// 	});
+	// }
+
+
+	//form.forEach((item, i) => {
+		//if(i % 2 === 0 ){}
+	form.forEach(item =>{
+		item.addEventListener('submit', ( e ) => {
+			e.preventDefault();
+
+			let statusMessage = document.createElement('div');
+			statusMessage.classList.add('status');
+			item.appendChild(statusMessage);
+
+			const formData = new FormData(item);
+			if(item.getAttribute('data-calc') === "end") {
+				for (let key in state) {
+					formData.append(key, state[key]);
+				}
+			}
+
+			postData('assets/server.php', formData)
+				.then(res => {
+					console.log(res);
+					statusMessage.textContent = message.success;
+			})
+			.catch(() => statusMessage.textContent = message.failure)
+			.finally(() => {
+					clearInputs();
+					setTimeout(() => {
+						statusMessage.remove();
+					}, 5000);
+			});
+		});
+	});
+};
+
+export default forms;
